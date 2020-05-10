54af7178038e8d0f9bb18b88cc29d5b49ec46644
Chanon Olley <olley102@users.noreply.github.com>
2020-05-10 15:05:32 +0100

    Update 4: Easier generalization with abc
    Loss class is now abstract (and a lot neater!), requiring loss and derivative to be overridden. CustomLoss has replaced the previous Loss class. Numerical derivatives no longer depend on the loss argument of __init__ in case of changes.
    
    Example of possible change:
    f = lambda x : x
    g = lambda x : f(x)
    del f

--- metrics.py
+++ metrics.py
@@ -1,26 +1,16 @@
 import numpy as np
+from abc import ABCMeta
+from abc import abstractmethod
 
-class Loss:
-    def __init__(self, loss=None, derivative=None, dp=0.1, centering=0):
-        if loss is not None:
-            self.loss = loss
-            if derivative is not None:
-                self.derivative = derivative
-            else:
-                # Symmetric derivative
-                if centering == 0:
-                    self.derivative = lambda p, y : (loss(p+0.5*dp, y) - loss(p-0.5*dp, y)) / dp
-                # Right derivative
-                elif centering == 1:
-                    self.derivative = lambda p, y : (loss(p+dp, y) - loss(p, y)) / dp
-                # Left derivative
-                elif centering == -1:
-                    self.derivative = lambda p, y : (loss(p, y) - loss(p-dp, y)) / dp
-                else:
-                    raise ValueError("Centering value is invalid. Must be integer of 0, 1 or -1.")
-        else:
-            self.loss = lambda p, y : (1/2) * ((p-y).transpose() @ (p-y)).diagonal()
-            self.derivative = lambda p, y : p - y
+
+class Loss(metaclass=ABCMeta):
+    @abstractmethod
+    def loss(self, p, y):
+        pass
+
+    @abstractmethod
+    def derivative(self, p, y):
+        pass
 
     def evaluate(self, pred, y):
         return self.loss(np.asmatrix(pred), np.asmatrix(y))
@@ -29,15 +19,47 @@ class Loss:
         return self.derivative(np.asmatrix(pred), np.asmatrix(y))
 
 
+class CustomLoss(Loss):
+    def __init__(self, loss=None, derivative=None, dp=0.1, centering=0):
+        if loss is not None:
+            self.custom_loss = loss
+            if derivative is not None:
+                self.custom_derivative = derivative
+            else:
+                # Symmetric derivative
+                if centering == 0:
+                    self.custom_derivative = \
+                        lambda p, y : (self.custom_loss(p+0.5*dp, y) - self.custom_loss(p-0.5*dp, y)) / dp
+                # Right derivative
+                elif centering == 1:
+                    self.custom_derivative = lambda p, y : (self.custom_loss(p+dp, y) - self.custom_loss(p, y)) / dp
+                # Left derivative
+                elif centering == -1:
+                    self.custom_derivative = lambda p, y : (self.custom_loss(p, y) - self.custom_loss(p-dp, y)) / dp
+                else:
+                    raise ValueError("Centering value is invalid. Must be integer of 0, 1 or -1.")
+        else:
+            self.custom_loss = MeanSquaredError().loss
+            self.custom_derivative = MeanSquaredError().derivative
+
+    def loss(self, p, y):
+        return self.custom_loss(p, y)
+
+    def derivative(self, p, y):
+        return self.custom_derivative(p, y)
+
+
 class MeanSquaredError(Loss):
-    def __init__(self):
-        loss = lambda p, y : (1/(2*y.shape[0])) * ((p-y).transpose() @ (p-y)).diagonal()
-        derivative = lambda p, y : (1/(y.shape[0])) * (p-y).sum(0)
-        super().__init__(loss=loss, derivative=derivative)
+    def loss(self, p, y):
+        return (1/(2*y.shape[0])) * ((p-y).transpose() @ (p-y)).diagonal()
+
+    def derivative(self, p, y):
+        return (1/(y.shape[0])) * (p-y).sum(0)
 
 
 class MeanAbsoluteError(Loss):
-    def __init__(self):
-        loss = lambda p, y : (1/(y.shape[0])) * np.abs(p-y).sum(0)
-        derivative = lambda p, y : (1/(y.shape[0])) * np.multiply(p-y, 1/np.abs(p-y)).sum(0)
-        super().__init__(loss=loss, derivative=derivative)
+    def loss(self, p, y):
+        return (1/(y.shape[0])) * np.abs(p-y).sum(0)
+
+    def derivative(self, p, y):
+        return (1/(y.shape[0])) * np.multiply(p-y, 1/np.abs(p-y)).sum(0)
