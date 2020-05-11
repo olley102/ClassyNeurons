Update 4.1: Correct backprop and update
Due to changes to backprop, gradient is a lot shallower, as it should be, when using sigmoid.

test_sequential_multilayer.py
@@ -3,15 +3,16 @@ from layers import Dense
 from layers import Sigmoid
 from models import Sequential
 from initializers import saved_weights
-from metrics import MeanSquaredError
+from metrics import SquaredError
+from metrics import CustomLoss
 import matplotlib.pyplot as plt
 
-bias_weights_0 = np.array([-5.1, 5])
-kernel_weights_0 = np.array([[5, -5.1],
-                             [5, -5.1]])
+bias_weights_0 = np.array([-5, 5])
+kernel_weights_0 = np.array([[5, -5],
+                             [5, -5]])
 
-bias_weights_1 = np.array([-5.1])
-kernel_weights_1 = np.array([[5.1],
+bias_weights_1 = np.array([-5])
+kernel_weights_1 = np.array([[5],
                              [5]])
 
 saved_bias_0 = saved_weights(bias_weights_0)
@@ -21,9 +22,9 @@ saved_bias_1 = saved_weights(bias_weights_1)
 saved_kernel_1 = saved_weights(kernel_weights_1)
 
 model = Sequential()
-model.add(Dense(2, 2, kernel_initializer=saved_kernel_0, bias_initializer=saved_bias_0, alpha=1.25))
+model.add(Dense(2, 2, kernel_initializer=saved_kernel_0, bias_initializer=saved_bias_0, alpha=50.0))
 model.add(Sigmoid())
-model.add(Dense(1, 2, kernel_initializer=saved_kernel_1, bias_initializer=saved_bias_1, alpha=1.25))
+model.add(Dense(1, 2, kernel_initializer=saved_kernel_1, bias_initializer=saved_bias_1, alpha=50.0))
 model.add(Sigmoid())
 
 X = np.array([[0, 0],
@@ -41,10 +42,11 @@ print(p)
 print("Error")
 print(p-y)
 
-loss_function = MeanSquaredError()
+loss_function = SquaredError()
+custom_loss = CustomLoss()
 
 print("Training")
-loss_history = model.fit(X, y, epochs=100, batch_size=4, steps_per_epoch=1000, halt=False, loss=loss_function)
+loss_history = model.fit(X, y, epochs=100, batch_size=4, steps_per_epoch=1000, halt=False, loss=custom_loss)
 print("Prediction")
 p = model.predict(X)
 print(p)