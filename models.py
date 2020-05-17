064f2bb3723b35ea0c09e48ae0a8ae349591ce03
Chanon Olley <olley102@users.noreply.github.com>
2020-05-17 17:28:14 +0100

    Update 5: Model save and restore
    Would you look at that.

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import dill
 from metrics import CustomLoss
 
 
@@ -67,8 +68,34 @@ class Sequential:
                 end = start + batch_size
                 loss_value = self.train_on_batch(X_copy[start:end, :], y_copy[start:end], loss=loss)
 
-            print("Losses:", loss_value)
+            print("Mean losses:", loss_value)
             loss_history = np.append(loss_history, loss_value, axis=0)
             ep += 1
 
         return loss_history
+
+    def save(self, file_path):
+        data = []
+
+        for layer in self.layers:
+            data.append(layer.get_save_data())
+
+        with open(file_path, "bw") as f:
+            dill.dump(data, f)
+
+        return "Model saved."
+
+    def restore(self, file_path):
+        with open(file_path, "br") as f:
+            data = dill.load(f)
+
+        self.layers = np.array([])
+
+        for layer_data in data:
+            assert isinstance(layer_data, dict)
+            layer_cls = layer_data["class"]
+            data_copy = layer_data.copy()
+            data_copy.pop("class")
+            self.layers = np.append(self.layers, layer_cls(**data_copy))
+
+        return "Model restored."
